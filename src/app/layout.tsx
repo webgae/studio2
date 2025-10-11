38b7229dfca6ec665877ba4cda501dc3c9766d67
Joaquin Sáez <ximosa@gmail.com>
2025-10-11 08:59:07 +0000

    EL ICONO DE LA PAGINA ES EL DE POR DEFECTO DE FIREBASE ESTUDIO PUEDES CA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
     template: '%s | WEBGAE',
   },
   description: 'Experto en WordPress dedicado a crear, mejorar y arreglar sitios web. Ofrezco soluciones de desarrollo, optimización y mantenimiento para potenciar tu presencia online.',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔖</text></svg>',
+  },
 };
 
 export default function RootLayout({
